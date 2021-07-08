73c437d0e9c9a9447167118e9eea6fae8198000e
NakUn <chnaan@gmail.com>
2021-07-08 15:46:16 +0900

    just do it

--- home/db/db.php
+++ home/db/db.php
@@ -220,7 +220,7 @@ class MyDB
     public function row(string $table, array $conds = [], $select = '*')
     {
         $rows = $this->rows($table, $conds, $select);
-        if ( ! $rows ) return [];
+        if ( ! $rows ) return null;
         return $rows[0];
     }
 
