47976f1798a67f44ec964cf6b0d35b4e7167cb2b
NakUn <chnaan@gmail.com>
2021-07-09 15:10:29 +0900

    try again

--- home/crud/users_crud.php
+++ home/crud/users_crud.php
@@ -11,7 +11,7 @@ $crud_tp 	= $_POST['crud_tp'];
 $user_info  = $_POST['user_info'];
 $where_map  = $_POST['where_map'];
 
-file_put_contents('myDebug',"'.input.','".$table."','".$crud_tp."','".$user_info."','".$where_map."',\n", FILE_APPEND | LOCK_EX);
+file_put_contents('debug.txt',"'.input==.','".$table."','".$crud_tp."','".$user_info."','".$where_map."',\n");
 if ($crud_tp == 'insert') {
     db()->insert($table, $user_info);
     echo $table.$user_info;
