ca923fd41206b6c49edb63c1155f1cc61ad9678e
NakUn <chnaan@gmail.com>
2021-07-29 15:36:36 +0900

    debug

--- home/crud/users_crud.php
+++ home/crud/users_crud.php
@@ -13,6 +13,9 @@ $where_map  = $_POST['where_map'];
 
 file_put_contents('debug.txt',"'.input==.','".$table."','".$crud_tp."','".$user_info."','".$where_map."',\n");
 file_put_contents('debug',"'.parseRecord.','".$crud_tp."','".$user_info."',\n", FILE_APPEND | LOCK_EX);
+
+alert(crud_tp);
+
 if ($crud_tp == 'insert') {
     db()->insert($table, $user_info);
     echo '1';
